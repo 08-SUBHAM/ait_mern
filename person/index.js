a79266892f27672b445194dd299634641186e41b
08-SUBHAM <145961492+08-SUBHAM@users.noreply.github.com>
2025-05-29 12:30:44 +0530

    made ejs file

--- person/index.js
+++ person/index.js
@@ -2,5 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const p1 = require('./p1');
+const port = 3000;
+const path = require('path');
+
+const app = express();
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 console.log("server working");
+
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+
