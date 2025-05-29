5445f665a10bfa33a763382f33045b25e96f8a45
08-SUBHAM <145961492+08-SUBHAM@users.noreply.github.com>
2025-05-29 13:03:22 +0530

    home page (unfinished)

--- person/index.js
+++ person/index.js
@@ -6,6 +6,10 @@ const port = 3000;
 const path = require('path');
 
 const app = express();
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
@@ -22,3 +26,6 @@ app.get("/", (req, res) => {
 });
 
 
+app.get("/about", (req, res) => {
+    res.render("about");
+});
